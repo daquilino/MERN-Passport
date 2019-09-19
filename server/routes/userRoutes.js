a5d068a9763a1e301ecc603b6c5c3582a375cc1e
D <douglasjaquilino@gmail.com>
2019-09-19 17:33:11 -0400

    added comments

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 
+// user signup  
 router.route("/signup")
 .post((req,res)=>res.send("signing up..."));
 
+// user login
 router.route("/login")
 .post((req,res)=>res.send("logging in..."));
 
+// user logout
 router.route("/logout")
 .post((req,res)=>res.send("logging OUT..."));
-
 
 
 module.exports = router;
